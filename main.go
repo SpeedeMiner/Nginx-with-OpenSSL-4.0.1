ad9f170abce1fe340485bc5929215c128f99e2da
SpeedeMiner <79132469+SpeedeMiner@users.noreply.github.com>
2026-08-25 20:37:27 +0500

    Update main.go

--- main.go
+++ main.go
@@ -623,7 +623,7 @@ func getOSINTDomains(ip string) []string {
 	var domains []string
 	client := &http.Client{Timeout: 4 * time.Second}
 	req, _ := http.NewRequest("GET", fmt.Sprintf("https://otx.alienvault.com/api/v1/indicators/IPv4/%s/passive_dns", ip), nil)
-	req.Header.Set("User-Agent", "Mozilla/5.0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36")
 	if resp, err := client.Do(req); err == nil {
 		defer resp.Body.Close()
 		var res struct {
@@ -989,7 +989,7 @@ func buildH2HeadersEncoder(sni string) []byte {
 	sniBytes := []byte(sni)
 	payload = append(payload, 0x01, byte(len(sniBytes)))
 	payload = append(payload, sniBytes...)
-	ua := []byte("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
+	ua := []byte("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36")
 	payload = append(payload, 0x0F, 0x2B, byte(len(ua)))
 	payload = append(payload, ua...)
 	return payload
